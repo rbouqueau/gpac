6cabef5b8195fab5a6eb9515d2576ad34ad30105
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2021-09-09 18:22:18 +0200

    osmo4_android_studio: debug build (but libgpac.so is still not debuggable)

--- build/android/jni/Application.mk
+++ build/android/jni/Application.mk
@@ -2,8 +2,8 @@ APP_ABI := armeabi-v7a x86
 APP_STL := c++_static
 APP_CPPFLAGS := -frtti -fexceptions -Wno-parentheses -Wno-shift-negative-value -Wno-pointer-sign
 APP_CFLAGS := -Wno-parentheses -Wno-shift-negative-value -Wno-pointer-sign
-LOCAL_CFLAGS := -O3
-APP_OPTIM := release
+LOCAL_CFLAGS := -O0 -g
+APP_OPTIM := debug
 
 
 APP_MODULES := gpac gm_ft_font libjavaenv gm_droid_out gm_droid_audio gm_droid_mpegv gpacWrapper mp4box
